209a6a9f922e2d81beb2fce052304b1b7a33670d
Hephzibah619 <hephzibah.ekerenam@pau.edu.ng>
2025-10-23 12:10:49 -0700

    (commit)

---
+++ week-4/practice_5/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+
+fn main() 
+{
+    let mut input = String::new();
+
+    println!("\nEnter your Height (in centimmeters):");
+    io::stdin().read_line(&mut input).expect("Not a valid string");
+    let height :f32 = input.trim().parse().expect("Not a valid number");
+
+    if height >= 150.0 && height <= 170.0
+    {
+        println!("you are of average height person");
+    }
+    else if height > 170.0 && height <= 195.0
+    {
+        println!("you are tall");
+    }
+    else if height <150.0 && height >100.0
+    {
+        println!("you are a dwarf");
+    }
+    else {
+        {
+            println!("Abnormal height");
+        }
+    }
+
+}
---
+++ week-4/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\h.ekerenamCOS101\week-4\practice_6\target\debug\practice_6.exe: C:\Users\HomePC\Documents\h.ekerenamCOS101\week-4\practice_6\src\main.rs
